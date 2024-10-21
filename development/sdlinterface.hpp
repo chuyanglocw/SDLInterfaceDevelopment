758ce2bc6218cfec4ae94d988445ce6c8b04ac9c
chuyanglocw <2659745557@qq.com>
2024-10-21 08:10:34 +0800

    添加具体事件处理

--- development/sdlinterface.hpp
+++ development/sdlinterface.hpp
@@ -6,11 +6,15 @@
 #ifndef _SDL_INTERFACE_H
 #define _SDL_INTERFACE_H
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_net.h>
 #undef main
 //定义过程函数
 void init();
 void render();
-bool handler(SDL_Event);
+bool handler(SDL_Event&);
 void eventloop();
 void dispolse();
 
